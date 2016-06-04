137b1c1557208e7a088fb4408bc3a8469c798506
esferaHQ <lsanchez@esferasoluciones.com>
2016-06-04 13:28:35 -0500

    relative

--- relatives/createRelative.php
+++ relatives/createRelative.php
@@ -3,10 +3,10 @@ include_once "../helper.php";
 header('Content-type: application/json;charset=utf-8');
 header("access-control-allow-origin: *");
 
-if(isset($_GET['lastname']) && isset($_GET['code']) && isset($_GET['contacttype']) && isset($_GET["contactdata"])){  
+if(isset($_POST['lastname']) && isset($_POST['code']) && isset($_POST['contacttype']) && isset($_POST["contactdata"])){  
     // primero revisar si el apellido y codigo son validos
-    $query = "SELECT * FROM operations WHERE code = ".$_GET['code'] . 
-            " AND patientid IN (SELECT id FROM patients WHERE lastname = '".$_GET['lastname']."')";
+    $query = "SELECT * FROM operations WHERE code = ".$_POST['code'] . 
+            " AND patientid IN (SELECT id FROM patients WHERE lastname = '".$_POST['lastname']."')";
     $result = mysql_query($query,$link) or die('Errant query:  '.$query); 
     
     $operations = array();
@@ -15,8 +15,8 @@ if(isset($_GET['lastname']) && isset($_GET['code']) && isset($_GET['contacttype'
     }
     if(count($operations)>0){
         // agregar el nuevo relative
-        $query = "INSERT INTO relatives (contactTypeId, contactData) VALUES (".$_GET['contacttype'].",'".
-                $_GET["contactdata"]."')";
+        $query = "INSERT INTO relatives (contactTypeId, contactData) VALUES (".$_POST['contacttype'].",'".
+                $_POST["contactdata"]."')";
         $result = mysql_query($query,$link) or die('Errant query:  '.$query);  
         // actualiza operations con el relative
         $query = "UPDATE operations SET relativeid = " .mysql_insert_id(). " WHERE id = " . $operations[0]->operation.id;
