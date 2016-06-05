34a6c2f6a0ecf09b8f8928976106f6e2f0340619
esferaHQ <lsanchez@esferasoluciones.com>
2016-06-04 22:26:35 -0500

    cambio

--- relatives/createRelative.php
+++ relatives/createRelative.php
@@ -16,12 +16,11 @@ if(isset($_GET['lastname']) && isset($_GET['code']) && isset($_GET['contacttype'
     }
     if(count($operations)>0){
         // agregar el nuevo relative
-        /*$query = "INSERT INTO relatives (contactTypeId, contactData) VALUES (".$_GET['contacttype'].",'".
+        $query = "INSERT INTO relatives (contactTypeId, contactData) VALUES (".$_GET['contacttype'].",'".
                 (isset($_GET["email"]) ? $_GET["email"] : $_GET["celphone"])."')";
-        $result = mysql_query($query,$link) or die('Errant query:  '.$query);  */
+        $result = mysql_query($query,$link) or die('Errant query:  '.$query); 
         // actualiza operations con el relative
-        /*$query = "UPDATE operations SET relativeid = " .mysql_insert_id(). " WHERE id = " . $operations[0]->operation.id;*/
-        print_r($operations[0]['operation']['id']);
+        $query = "UPDATE operations SET relativeid = " .mysql_insert_id(). " WHERE id = " . $operations[0]->operation.id;
         echo json_encode(array('status'=>'success'));    
     }
     else{
