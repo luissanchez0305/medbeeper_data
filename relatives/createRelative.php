5fa81c2f1a9aac4a597c05fd0256dd4e1ff82494
esferaHQ <lsanchez@esferasoluciones.com>
2016-06-05 00:16:03 -0500

    cambio

--- relatives/createRelative.php
+++ relatives/createRelative.php
@@ -16,6 +16,7 @@ if(isset($_GET['lastname']) && isset($_GET['code']) && isset($_GET['contacttype'
     }
     if(count($operations)>0){
         // agregar el nuevo relative
+        echo '-' . (count($_GET["email"]) > 0 ? $_GET["email"] : $_GET["celphone"]) . '.';
         $query = "INSERT INTO relatives (contactTypeId, contactData) VALUES (".$_GET['contacttype'].",'".
                 (count($_GET["email"]) > 0 ? $_GET["email"] : $_GET["celphone"])."')";
         $result = mysql_query($query,$link) or die('Errant query:  '.$query); 
