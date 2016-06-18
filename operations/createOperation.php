5f16dc9ef0059bc5610f11c26ffddb572568078a
esferaHQ <lsanchez@esferasoluciones.com>
2016-06-17 22:19:59 -0500

    create operation

--- operations/createOperation.php
+++ operations/createOperation.php
@@ -3,10 +3,24 @@ include_once "../helper.php";
 header('Content-type: application/json;charset=utf-8');
 header("access-control-allow-origin: *");
 
-if(isset($_GET['patientId']) && isset($_GET['doctorId']) && isset($_GET['operationName'])){    
-    $query = "INSERT INTO operations (patientid, doctorid, name, code) VALUES (".$_GET['patientId'].",".
-            $_GET['doctorId'].",'".$_GET['operationName']."','".generateRandomString()."')";
-    $result = mysql_query($query,$link) or die('Errant query:  '.$query);  
+if(isset($_GET['lastname']) && isset($_GET['doctorId'])){    
+    $query = "INSERT INTO patients (name, lastname, doctorid) VALUES ('" . $_GET['name'] . "', '" . $_GET['lastname'] . "',".
+            $_GET['doctorId'] . ")";
+    $result = mysql_query($query,$link) or die('Errant query:  ' . $query); 
+    
+    $query = "INSERT INTO operations (patientid, doctorid, name, code) VALUES (" . mysql_insert_id() . ",".
+            $_GET['doctorId'].",'" . $_GET['operationName'] . "','" . generateRandomString() . "')";
+    $result = mysql_query($query,$link) or die('Errant query:  ' . $query); 
+    
+    $query = "SELECT code FROM operations WHERE id = " . mysql_insert_id();
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query); 
+
+    $operations = array();
+    while($operation = mysql_fetch_assoc($result)) {
+        $operations[] = array('operation'=>$operation);    
+    }
+    echo json_encode(array('status'=>'success', 'code'=>$patients[0]['operation']['code']));
+     
 }
 
 function generateRandomString($length = 10) {
